31f854efbfd6dbd5d56a5caf76ee457be7d71afd
Justin Alvarez <alvajus@amazon.com>
2025-05-06 19:16:47 +0000

    switch deprecated import of github.com/containerd/containerd/platforms to github.com/containerd/platforms
    Signed-off-by: Justin Alvarez <alvajus@amazon.com>

--- daemon/containerd/image.go
+++ daemon/containerd/image.go
@@ -11,9 +11,9 @@ import (
 	"time"
 
 	containerdimages "github.com/containerd/containerd/images"
-	"github.com/containerd/containerd/platforms"
 	cerrdefs "github.com/containerd/errdefs"
 	"github.com/containerd/log"
+	"github.com/containerd/platforms"
 	"github.com/distribution/reference"
 	imagetype "github.com/docker/docker/api/types/image"
 	"github.com/docker/docker/daemon/images"
